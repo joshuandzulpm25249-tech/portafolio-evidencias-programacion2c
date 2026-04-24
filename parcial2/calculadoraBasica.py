89434f27dadf93e99a315d33ec19dbed91a465c8
joshuandzulpm25249-tech <joshuan.dzul_pm25249@cecyte.edu.mx>
2026-04-24 12:23:43 -0600

    Update calculadoraBasica.py

--- parcial2/calculadoraBasica.py
+++ parcial2/calculadoraBasica.py
@@ -10,6 +10,7 @@ def multiplicacion(a,b):
 
 def divicion(a,b):
     return a / b
+    
 #menu de opcionesdef suma(a,b):
 opcion = 0
 while(opcion != 5):
@@ -26,21 +27,25 @@ while(opcion != 5):
         n1 = float(input("\nIngresa el primer numero a sumar: "))
         n2 = float(input("\nIngresa el segundo numero a sumar: "))
         print(f"diferencia de la suma: {suma(n1,n2)}")
+        
     elif opcion == 2:
         #resta de 2 numetos
         n1 = float(input("\nIngresa minuendo: "))
         n2 = float(input("\nIngresa el sustrayendo: "))
         print(f"cociente de la resta: {resta(n1,n2)}")
+        
     elif opcion == 3:
         #multiplicacion de 2 numeros
         n1 = float(input("\nIngresa el primer factor: "))
         n2 = float(input("\nIngresa el segundo factor: "))
         print(f"producto resultante: {multiplicacion(n1,n2)}")
+        
     elif opcion == 4:
         #divicion de 2 numeros
         n1 = float(input("\nIngresa el dividendo "))
         n2 = float(input("\nIngresa el divisor: "))
         print(f"cociente resultado: {divicion(n1,n2)}")
+        
     elif opcion == 5:
         #finalizacion del programa
         print("programa finalizado...")
