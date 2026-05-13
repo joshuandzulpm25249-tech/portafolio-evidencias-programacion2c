c24e2f30d31433356e937fa4d189e660461969ca
Joshuan Dzul <joshuan.dzul_pm25249@cecyte.edu.mx>
2026-05-13 15:44:00 -0600

    se agregan ejercicios del parcial 3

--- ejercicios/joshuan-dzul/parcial3/persona.py
+++ ejercicios/joshuan-dzul/parcial3/persona.py
@@ -1,8 +1,11 @@
 """
-crea una clase persona con ls siguientes atributos: nombre, edad, genero y nacionalidad
-Agrega un metodo para imprimir los datos de la persona y otro metodo para calcular el año
+crea una clase persona con ls siguientes atributos: nombre, edad, genero y
+ nacionalidad
+Agrega un metodo para imprimir los datos de la persona y otro metodo para
+ calcular el año
 de nacimiento de la persona.
-crea un objeto de la clase personal y utiliza los metodos para mostrar su informacion y
+crea un objeto de la clase personal y utiliza los metodos para mostrar
+ su informacion y
 calcula su año de nacimiento
 """
 import datetime
@@ -31,4 +34,4 @@ def main():
     print(f"Año de nacimiento:  {objPersona.calcularNacimiento()}")
 
 if __name__ == "__main__":
-main()  
+    main()
